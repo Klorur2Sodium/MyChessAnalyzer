399384b3e2c08e6865db2ddf5cfcd497af707d3d
Victor Fraize <vfraize3@gmail.com>
2025-05-04 18:26:01 +0200

    Basic project config

---
+++ src/main/java/com/goofbass/GoofbassApplication.java
@@ -0,0 +1,13 @@
+package com.goofbass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoofbassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoofbassApplication.class, args);
+	}
+
+}
